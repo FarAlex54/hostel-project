638107ea9987526df7cd2e83b5234757271bc88a
FarAlex <sibacadem@mail.ru>
2023-07-28 12:07:33 +0700

    добавление отзывов с формой

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,47 +1,43 @@
 import React from 'react'
 import './styles/Feedback.css';
 import FeedBackItem from './cart/FeedBackItem';
-import { useForm } from "react-hook-form"
 import DtPicker from 'react-calendar-datetime-picker'
 import { useState, useRef } from 'react';
 import { AppContext } from '../App';
 import axios from 'axios';
-
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 
 const Feedback = (props) => {
   
   const context = React.createContext(AppContext)
 
- 
-
-  const onSubmit = data => console.log(data)
+  const [showModal, setShowModal] = useState(false);
+  const [isReadOnly, setIsReadOnly] = useState(true)
   const [dateVisit,setDateVisit] = useState(null);
   const [rating,setRating] = useState(0);
   const [hover,setHover] = useState(null);
   const [plus,setPlus] = useState('');
   const [minus,setMinus] = useState('');
   const [name,setName] = useState('Модуляр');
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+  let timelessVar=0;
+
   function handleSubmit(e){
     e.preventDefault()  // без перезагрузки страницы
-    if (plus===''){
-      console.log('Plus is null')
-
-    }
-    else{
-      console.log('Plus is NOT null')
-      /* axios.post('https://64775eca9233e82dd53b8a86.mockapi.io/feedback', {
+       axios.post('https://64775eca9233e82dd53b8a86.mockapi.io/feedback', {
         name:name,
         plus:plus,
         minus:minus,
         rating:rating,
         living_date:dateVisit.day+'/'+dateVisit.month+'/'+dateVisit.year
-      }) */
-    }
-    setPlus(''); setMinus(''); setRating(''); setDateVisit(null);
-  }
-  
+      })
+      setPlus(''); setMinus(''); setRating(0); setDateVisit(null);setIsReadOnly(true);setShowModal(true);
 
+  }
 
    return (
 
@@ -53,47 +49,64 @@ const Feedback = (props) => {
                                /> 
         </div>
         <div class="p-2 col-4">
-          <form class="rounded p-2" id='feedbackForm' onSubmit={handleSubmit}>
+          <form class="rounded p-2" id='feedbackForm' onChange={()=>{(timelessVar&&plus.length) > 0 ? setIsReadOnly(false) : setIsReadOnly(true)}} onSubmit={handleSubmit}>
             <h5 class=''>Оставте Ваш отзыв</h5>
                 <div class="form-floating">
-                  <textarea class="form-control" style={{height: 15 +'em'}} id="Plus" value={plus} onChange={(e) => {setPlus(e.target.value)}}></textarea>
+                   <textarea class="form-control" style={{height: 15 +'em'}} id="Plus" value={plus} onChange={(e) => {setPlus(e.target.value)}}></textarea>
                   <label for="Plus">Что понравилось?</label>
                 </div>
                 <div class="form-floating py-2">
                   <textarea class="form-control" style={{height: 10 +'em'}} id="Minus" value={minus} onChange={(e) => {setMinus(e.target.value)}}></textarea>
+
                   <label for="Minus">Что не понравилось?</label>
                 </div>
-                <div>
-                  <p class='px-2'>Оценка:
-                    {[...Array(5)].map((star, i)=>{
-                      const ratingValue = i + 1;
-                      const container ={
-                        color: (ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9')             
-                      }
-                      return <label>
-                              <input 
-                                type="radio" 
-                                name="rating" 
-                                value={ratingValue} 
-                                onClick={()=>setRating(ratingValue)}
-                              />
-                              <i class="bi-star-fill star"
-                                style={container}
-                                onMouseEnter={()=>setHover(ratingValue)}
-                                onMouseLeave={()=>setHover(null)}
-                              ></i>
-                            </label>
-                    })}
-                  </p>
-                </div>              
-                <DtPicker placeholder='Дата заезда' className='calendar' onChange={setDateVisit} showWeekend/>
-            <input type="submit"/>
+                <div class='d-flex flex-row justify-content-between align-items-center'>
+                  <div>
+                    <p class='px-2' >Оценка: 
+                      {[...Array(5)].map((star, i)=>{
+                        const ratingValue = i + 1;
+                        const container ={
+                          color: (ratingValue <= (hover || rating) ? '#ffc107' : '#e4e5e9')             
+                        }                      
+                        return <label>
+                                <input 
+                                  type="radio" 
+                                  name="rating" 
+                                  value={ratingValue} 
+                                  onClick={()=>{timelessVar=ratingValue;setRating(ratingValue)}}
+                                />
+  
+                                <i class="bi-star-fill star"
+                                  style={container}
+                                  onMouseEnter={()=>setHover(ratingValue)}
+                                  onMouseLeave={()=>setHover(null)}
+                                ></i>
+                              </label>
+                      })}
+                    </p>
+                  </div>
+                  <div>
+                    <DtPicker placeholder='Дата проживания' className='calendar' onChange={setDateVisit} showWeekend/>  
+                  </div>
+                </div>
 
-                        
+                  <input type="submit" class='btn btn-info' disabled={isReadOnly}/>
+
+
           </form>
         </div>  
+        <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>Спасибо, {name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Ваш отзыв принят в проверку</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
       </div>
-
   )
 }
 
---
+++ src/components/cart/renderTooltip.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const renderTooltip = (props) => (
+  <Tooltip id="button-tooltip" {...props}>
+    dfghfhfhf{props.plus}
+  </Tooltip>
+);
+const renderTooltip = () => {
+  return (
+    <div>renderTooltip</div>
+  )
+}
+
+export default renderTooltip
